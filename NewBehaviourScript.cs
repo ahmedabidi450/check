68d45b0f85e4604dcc705053b7e311cf3d8f949f
ahmedabidi450 <54631055+ahmedabidi450@users.noreply.github.com>
2019-09-02 14:01:01 +0100

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void Multiply(int a, int b)
+        {
+       int c = a * b;
+        Debug.Log(c);
+        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Multiply(25, 8);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
